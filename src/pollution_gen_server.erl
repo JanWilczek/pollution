97c9e956413ff9ee75c83f67d9b9410e7f771060
Jan Wilczek <janekw@vp.pl>
2019-06-02 10:21:46 +0200

    getStationMean added to gen_server.

--- src/pollution_gen_server.erl
+++ src/pollution_gen_server.erl
@@ -12,7 +12,7 @@
 
 %% API
 -export([start_link/1, init/1, handle_cast/2, handle_call/3, terminate/2]).
--export([start/0, stop/0, addStation/2, addValue/4, removeValue/3, getOneValue/3]).
+-export([start/0, stop/0, addStation/2, addValue/4, removeValue/3, getOneValue/3, getStationMean/2]).
 
 start_link(InitialValue) ->     % server name           callback module
   gen_server:start_link({local, pollution_gen_server}, pollution_gen_server, InitialValue, []).
@@ -34,18 +34,15 @@ addStation(StationName, StationCoordinates) ->
 addValue(StationNameOrCoordinates, DateTime, Type, Value) ->
   gen_server:call(pollution_gen_server, {addValue, StationNameOrCoordinates, DateTime, Type, Value}).
 
-getOneValue(StationNameOrCoordinates, Date, Type) ->
-  gen_server:call(pollution_gen_server, {getOneValue, StationNameOrCoordinates, Date, Type}).
-
 removeValue(StationNameOrCoordinates, DateTime, Type) ->
   gen_server:call({removeValue, StationNameOrCoordinates, DateTime, Type}).
 
-%%getOneValue(StationNameOrCoordinates, DateTime, Type) ->
-%%  call({getOneValue, StationNameOrCoordinates, DateTime, Type}).
-%%
-%%getStationMean(StationNameOrCoordinates, Type) ->
-%%  call({getStationMean, StationNameOrCoordinates, Type}).
-%%
+getOneValue(StationNameOrCoordinates, Date, Type) ->
+  gen_server:call(pollution_gen_server, {getOneValue, StationNameOrCoordinates, Date, Type}).
+
+getStationMean(StationNameOrCoordinates, Type) ->
+  gen_server:call({getStationMean, StationNameOrCoordinates, Type}).
+
 %%getDailyMean(Date, Type) ->
 %%  call({getDailyMean, Date, Type}).
 %%
@@ -78,7 +75,9 @@ handle_call({removeValue, StationNameOrCoordinates, DateTime, Type}, _From, Moni
   case NewMonitor of
     {error, _} -> {reply, NewMonitor, Monitor};
     _ -> {reply, ok, NewMonitor}
-  end.
+  end;
+handle_call({getStationMean, StationNameOrCoordinates, Type}, _From, Monitor) ->
+  {reply, pollution:getStationMean(Monitor, StationNameOrCoordinates, Type), Monitor}.
 
 terminate(Reason, Value) ->
   io:format("Server: exit with value ~p~n.", [Value]),
